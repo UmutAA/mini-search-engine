ea634c48b134ed0da076c1c01b74b2f116f45b19
UmutAA <201361309+UmutAA@users.noreply.github.com>
2026-08-11 19:23:34 +0300

    implement build_index() function

--- src/indexer.py
+++ src/indexer.py
@@ -1,15 +1,18 @@
+from typing import List
 from pathlib import Path
+from collections import defaultdict
 from tokenizer import tokenize
 
-def open_docs():
-    base_dir = Path(__file__).resolve().parent.parent
-    path = base_dir / "tests"
+def open_docs() -> dict[str, List[str]]:
+    documents = {}
+    base_dir = Path(__file__).resolve().parent.parent # Get projects absolute directory
+    path = base_dir / "tests" # Access txt files' directory
     try:
         for file_path in path.glob("*.txt"):
             with open(file_path, "r", encoding="utf-8") as file:
                 text = file.read()
-                tokens = tokenize(text)
-                print(tokens)
+                tokens = tokenize(text) # Tokenize string
+                documents[file_path.name] = tokens # Add tokens to the document dictionary
 
     except FileNotFoundError:
         print("Error: File Not Found")
@@ -17,4 +20,11 @@ def open_docs():
     except Exception as e:
         print(f"Error: {e}")
 
+    return documents
 
+def build_index(documents: dict[str,List[str]]) -> dict[str, set[str]]:
+    index = defaultdict(set)
+    for document, tokens in documents.items():
+        for token in tokens:
+            index[token].add(document)
+    return index
