6ad54766df58418ee221e15261be78f779cd3b53
UmutAA <201361309+UmutAA@users.noreply.github.com>
2026-08-11 19:42:05 +0300

    implemented search(query, index) function

--- src/searcher.py
+++ src/searcher.py
@@ -1,2 +1,12 @@
-def search():
-    pass
+from tokenizer import tokenize
+
+def search(query: str, index: dict[str, set[str]]) -> set[str]:
+    documents = set()
+    query_tokens = tokenize(query) 
+    for query_token in query_tokens:
+        if query_token not in index:
+            continue
+        
+        for doc in index[query_token]:
+            documents.add(doc)
+    return documents
